876a2340ac5ca0d92cac35e76514f2b33ee52953
Basti Schwarz <53933323+bastischwarz-96@users.noreply.github.com>
2022-01-10 17:29:45 +0200

    adding all constraints
    adding all constraints, deleting paymentoverview --> rebranding to missing screen

--- app/src/main/java/com/example/myapplication/ActivityAddPayment.java
+++ app/src/main/java/com/example/myapplication/ActivityAddPayment.java
@@ -3,11 +3,9 @@ package com.example.myapplication;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
-<<<<<<< HEAD
+
 import android.view.View;
 import android.widget.Button;
-=======
->>>>>>> d3258c5169dad42f6db417499befed5ddef50bb8
 
 public class ActivityAddPayment extends AppCompatActivity {
 
