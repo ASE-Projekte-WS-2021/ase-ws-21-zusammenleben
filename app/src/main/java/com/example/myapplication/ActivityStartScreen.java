a37f7adc0e489d7fe780b540abc55e01852813b6
sonasjoehnlein <jonas.soehnlein@stud.uni-regensburg.de>
2022-01-10 18:24:54 +0100

    add ButtonListener

--- app/src/main/java/com/example/myapplication/ActivityStartScreen.java
+++ app/src/main/java/com/example/myapplication/ActivityStartScreen.java
@@ -2,14 +2,41 @@ package com.example.myapplication;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
 
 public class ActivityStartScreen extends AppCompatActivity {
+
+    Button button;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //try to setup and test my stuff
         setContentView(R.layout.activity_startscreen);
+        addListenerOnButton();
+    }
+
+    public void addListenerOnButton() {
+
+        final Context context = this;
+
+        button = (Button) findViewById(R.id.button2);
+
+        button.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent intent = new Intent(context, ActivityOverview.class);
+                startActivity(intent);
+
+            }
+
+        });
+
     }
 }
