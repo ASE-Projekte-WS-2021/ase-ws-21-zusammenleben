b355224c4775e4274f35caa496854aea7d6305d4
sonasjoehnlein <jonas.soehnlein@stud.uni-regensburg.de>
2022-01-17 16:58:17 +0100

    fill signup class

--- app/src/main/java/com/example/myapplication/ActivitySignup.java
+++ app/src/main/java/com/example/myapplication/ActivitySignup.java
@@ -1,14 +1,42 @@
 package com.example.myapplication;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ActivitySignup extends AppCompatActivity {
 
+    Button create_account;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
+        addListenerOnButton();
     }
+
+    public void addListenerOnButton() {
+
+        final Context context = this;
+
+        create_account = (Button) findViewById(R.id.create_Account);
+
+        create_account.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent intent = new Intent(context, ActivityStartScreen.class);
+                startActivity(intent);
+
+            }
+
+        });
+    }
+
 }
