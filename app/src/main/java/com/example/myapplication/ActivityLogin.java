c88de329b35657026f86ffd202052246c280ad2a
sonasjoehnlein <jonas.soehnlein@stud.uni-regensburg.de>
2022-01-17 15:19:38 +0100

    signup class

--- app/src/main/java/com/example/myapplication/ActivityLogin.java
+++ app/src/main/java/com/example/myapplication/ActivityLogin.java
@@ -1,15 +1,43 @@
 package com.example.myapplication;
 
+import android.content.Context;
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ActivityLogin extends AppCompatActivity {
 
+    TextView signup;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+        addListenerOnButton();
+
+    }
+
+    public void addListenerOnButton() {
+
+        final Context context = this;
+
+        signup = (TextView) findViewById(R.id.login_signup);
+
+        signup.setOnClickListener(new View.OnClickListener() {
+
+            @Override
+            public void onClick(View arg0) {
+
+                Intent intent = new Intent(context, ActivitySignup.class);
+                startActivity(intent);
+
+            }
+
+        });
 
     }
 }
