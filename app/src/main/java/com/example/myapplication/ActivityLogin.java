badc2d7de9545c14eefc5f44198fb5e7a123c175
sonasjoehnlein <jonas.soehnlein@stud.uni-regensburg.de>
2022-01-24 20:16:37 +0100

    refresh code ActivitiyLogin

--- app/src/main/java/com/example/myapplication/ActivityLogin.java
+++ app/src/main/java/com/example/myapplication/ActivityLogin.java
@@ -5,12 +5,14 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ActivityLogin extends AppCompatActivity {
 
+    EditText username, password;
     TextView signup;
     Button login;
 
@@ -18,39 +20,29 @@ public class ActivityLogin extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
-        addListenerOnButton();
 
-    }
-
-    public void addListenerOnButton() {
-
-        final Context context = this;
-
+        username = (EditText) findViewById(R.id.login_username);
+        password = (EditText) findViewById(R.id.login_password);
         signup = (TextView) findViewById(R.id.login_signup);
         login = (Button) findViewById(R.id.login);
 
         signup.setOnClickListener(new View.OnClickListener() {
-
             @Override
-            public void onClick(View arg0) {
+            public void onClick(View view) {
 
-                Intent intent = new Intent(context, ActivitySignup.class);
+                Intent intent = new Intent(getApplicationContext(),ActivitySignup.class);
                 startActivity(intent);
-
             }
-
         });
 
         login.setOnClickListener(new View.OnClickListener() {
-
             @Override
-            public void onClick(View arg0) {
+            public void onClick(View view) {
 
-                Intent intent = new Intent(context, ActivityStartScreen.class);
+                Intent intent = new Intent(getApplicationContext(), ActivityStartScreen.class);
                 startActivity(intent);
 
             }
-
         });
 
     }
