4b1dd9fbfb5a3a11b608d2e8b0c97260f6782bae
sonasjoehnlein <jonas.soehnlein@stud.uni-regensburg.de>
2022-01-24 20:27:39 +0100

    refresh code ActivitySignup

--- app/src/main/java/com/example/myapplication/ActivitySignup.java
+++ app/src/main/java/com/example/myapplication/ActivitySignup.java
@@ -5,37 +5,34 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
+import android.widget.EditText;
 import android.widget.TextView;
 
 import androidx.appcompat.app.AppCompatActivity;
 
 public class ActivitySignup extends AppCompatActivity {
 
+    EditText username, password, e_mail, repeat_password;
     Button create_account;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_signup);
-        addListenerOnButton();
-    }
 
-    public void addListenerOnButton() {
-
-        final Context context = this;
-
+        username = (EditText) findViewById(R.id.signup_username);
+        e_mail = (EditText) findViewById(R.id.signup_e_mail);
+        password = (EditText) findViewById(R.id.signup_password);
+        repeat_password = (EditText) findViewById(R.id.signup_repassword);
         create_account = (Button) findViewById(R.id.create_Account);
 
         create_account.setOnClickListener(new View.OnClickListener() {
-
             @Override
-            public void onClick(View arg0) {
+            public void onClick(View view) {
 
-                Intent intent = new Intent(context, ActivityStartScreen.class);
+                Intent intent = new Intent(getApplicationContext(),ActivityStartScreen.class);
                 startActivity(intent);
-
             }
-
         });
     }
 
