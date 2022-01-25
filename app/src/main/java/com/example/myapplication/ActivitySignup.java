d04813e24766d1449a2668669a8c1c890e129c98
sonasjoehnlein <jonas.soehnlein@stud.uni-regensburg.de>
2022-01-25 13:47:35 +0100

    Signup Process

--- app/src/main/java/com/example/myapplication/ActivitySignup.java
+++ app/src/main/java/com/example/myapplication/ActivitySignup.java
@@ -7,6 +7,7 @@ import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
 import android.widget.TextView;
+import android.widget.Toast;
 
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -14,6 +15,7 @@ public class ActivitySignup extends AppCompatActivity {
 
     EditText username, password, e_mail, repeat_password;
     Button create_account;
+    LoginDBHelper UserDB;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -25,13 +27,37 @@ public class ActivitySignup extends AppCompatActivity {
         password = (EditText) findViewById(R.id.signup_password);
         repeat_password = (EditText) findViewById(R.id.signup_repassword);
         create_account = (Button) findViewById(R.id.create_Account);
+        UserDB = new LoginDBHelper(this);
 
         create_account.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View view) {
 
-                Intent intent = new Intent(getApplicationContext(),ActivityStartScreen.class);
-                startActivity(intent);
+                String user = username.getText().toString();
+                String pass = password.getText().toString();
+                String repass = repeat_password.getText().toString();
+
+                if (user.equals("")||pass.equals("")||repass.equals(""))
+                    Toast.makeText(ActivitySignup.this, "Bitte füllen Sie alle notwendige Angaben aus", Toast.LENGTH_SHORT).show();
+                else{
+                    if (pass.equals(repass)){
+                        Boolean checkuser = UserDB.checkusername(user);
+                        if (checkuser==false){
+                             Boolean insert = UserDB.insertData(user, pass);
+                             if (insert==true){
+                                 Toast.makeText(ActivitySignup.this, "Registrierung erfolgreich", Toast.LENGTH_SHORT).show();
+                                 Intent intent = new Intent(getApplicationContext(),ActivityStartScreen.class);
+                                 startActivity(intent);
+                             } else{
+                                 Toast.makeText(ActivitySignup.this, "Registrierung fehlgeschlagen", Toast.LENGTH_SHORT).show();
+                             }
+                        } else{
+                            Toast.makeText(ActivitySignup.this, "Der Benutzer existiert bereits. Bitte melden Sie sich an!", Toast.LENGTH_SHORT).show();
+                        }
+                    } else{
+                        Toast.makeText(ActivitySignup.this, "Die Passwörter stimmen nicht überein.", Toast.LENGTH_SHORT).show();
+                    }
+                }
             }
         });
     }
