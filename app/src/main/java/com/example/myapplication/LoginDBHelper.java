0bdf8c6276435d5976aa55d36952af83fd0ad833
sonasjoehnlein <jonas.soehnlein@stud.uni-regensburg.de>
2022-01-25 11:21:39 +0100

    add HelperClass for Login Database

---
+++ app/src/main/java/com/example/myapplication/LoginDBHelper.java
@@ -0,0 +1,26 @@
+package com.example.myapplication;
+
+import android.content.Context;
+import android.database.sqlite.SQLiteDatabase;
+import android.database.sqlite.SQLiteOpenHelper;
+
+import androidx.annotation.Nullable;
+
+public class LoginDBHelper extends SQLiteOpenHelper {
+
+    public static final String DBNAME = "UserInformation.db";
+
+    public LoginDBHelper(Context context) {
+        super(context, "UserInformation.db", null, 1);
+    }
+
+    @Override
+    public void onCreate(SQLiteDatabase sqLiteDatabase) {
+
+    }
+
+    @Override
+    public void onUpgrade(SQLiteDatabase sqLiteDatabase, int i, int i1) {
+
+    }
+}
