a0a1f7f8c67d06c797f6d8a75ae061892799bc7f
Basti Schwarz <53933323+bastischwarz-96@users.noreply.github.com>
2022-01-31 14:06:34 +0100

    test

--- app/src/main/java/com/example/myapplication/ActivityPaymentOverview.java
+++ app/src/main/java/com/example/myapplication/ActivityPaymentOverview.java
@@ -6,6 +6,7 @@ import android.content.Context;
 import android.content.Intent;
 import android.os.Bundle;
 import android.renderscript.ScriptGroup;
+import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -13,11 +14,13 @@ import android.widget.TextView;
 
 public class ActivityPaymentOverview extends AppCompatActivity {
 
+    String purpose;
+    int costs;
+
     Button button_savepayment;
     EditText input_purpose, input_costs;
 
-    TextView testtext = (TextView) findViewById(R.id.share_your_bill);
-
+    TextView testtext;
 
 
     @Override
@@ -26,10 +29,20 @@ public class ActivityPaymentOverview extends AppCompatActivity {
         //try to setup and test my stuff
         setContentView(R.layout.activity_paymentoverview);
         addListenerOnButton();
-        getInputCosts();
-        getInputPurpose();
+
+        input_costs = (EditText)findViewById(R.id.insert_costs);
+        input_purpose = (EditText)findViewById(R.id.insert_purpose);
+        testtext = (TextView) findViewById(R.id.share_your_bill);
+
         //displaytext();
+        //checkForUserInput();
     }
+/*
+    @Override
+    protected void onStart(){
+        super.onStart();
+        checkForUserInput();
+    }*/
 
     public void addListenerOnButton() {
 
@@ -40,30 +53,48 @@ public class ActivityPaymentOverview extends AppCompatActivity {
         button_savepayment.setOnClickListener(new View.OnClickListener() {
 
             @Override
-            public void onClick(View arg0) {
+            public void onClick(View view) {
                 //get and save input from input_purpose and input_costs into db
-                //Intent intent = new Intent(context, ActivityPaymentOverview.class);
-                //startActivity(intent);
-                //testtext.setText(getInputCosts()+":"+getInputPurpose());
-                ActivityPaymentOverview.this.startActivity(new Intent(ActivityPaymentOverview.this, ActivityOverview.class));
+                Intent intent = new Intent(context, ActivityOverview.class);
+                startActivity(intent);
+                //ActivityPaymentOverview.this.startActivity(new Intent(ActivityPaymentOverview.this, ActivityOverview.class));
             }
 
         });
 
     }
+/*
+    public void checkForUserInput (){
+
+        if (getInputCosts().isEmpty()){
+            Log.i("myApp", "Empty");
+        }
+        else {getInputCosts();}
+
+        if (getInputPurpose().isEmpty()){
+            Log.i("myApp", "EmptyP");
+        }
+        else { getInputPurpose();}
+        displaytext();
+    }
 
     public String getInputCosts(){
-        input_costs = (EditText)findViewById(R.id.insert_costs);
-        return input_costs.getText().toString();
+        return String.valueOf(costs = Integer.valueOf(input_costs.getText().toString()));
     }
 
     public String getInputPurpose(){
-        input_purpose = (EditText)findViewById(R.id.insert_purpose);
-        return input_purpose.getText().toString();
+        return purpose = input_purpose.getText().toString();
     }
-/*
+
     public void displaytext(){
         testtext.setText(getInputCosts()+":"+getInputPurpose());
+    }
+
+    @Override
+    public void onDestroy() {
+        // stop the Service
+        stopService(new Intent(this, ActivityPaymentOverview.class));
+        super.onDestroy();
     }
 */
 }
