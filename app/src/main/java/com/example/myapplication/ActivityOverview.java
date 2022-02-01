6c74374d972409fc6be0bee593b1dbf9c7144f1f
MarcoBeetz23 <57911367+MarcoBeetz23@users.noreply.github.com>
2022-02-01 20:51:49 +0100

    -u

--- app/src/main/java/com/example/myapplication/ActivityOverview.java
+++ app/src/main/java/com/example/myapplication/ActivityOverview.java
@@ -9,9 +9,12 @@ import android.view.View;
 import android.widget.ImageButton;
 import android.widget.TextView;
 
+import org.w3c.dom.Text;
+
 public class ActivityOverview extends AppCompatActivity {
 
     ImageButton button_managePayments;
+    PaymentMemo payment;
     //TextView testtext;
 
     @Override
@@ -21,6 +24,34 @@ public class ActivityOverview extends AppCompatActivity {
         setContentView(R.layout.activity_overview);
         addListenerOnButton();
         //displaytext();
+    }
+
+    @Override
+    protected void onResume() {
+        super.onResume();
+        PaymentMemoDataSource db = new PaymentMemoDataSource(this);
+        try {
+            payment = db.getPaymentMemo();
+            updateTextView();
+        } catch (android.database.CursorIndexOutOfBoundsException e){
+            System.out.println("Database stoll empty...");
+        }
+        System.out.println(payment);
+
+    }
+
+    private void updateTextView() {
+        String s = payment.toString();
+        System.out.println(s);
+        String[] substring = s.split("#");
+        String cost = substring[0];
+        String product = substring[1];
+        System.out.println(cost);
+        System.out.println(product);
+        TextView description = findViewById(R.id.payment_purpose);
+        TextView costs = findViewById(R.id.costs);
+        description.setText(product);
+        costs.setText(cost);
     }
 
     public void addListenerOnButton() {
@@ -33,9 +64,9 @@ public class ActivityOverview extends AppCompatActivity {
 
             @Override
             public void onClick(View view) {
-                ActivityOverview.this.startActivity(new Intent(ActivityOverview.this, ActivityPaymentOverview.class));
-                //Intent intent = new Intent(getApplicationContext(), ActivityPaymentOverview.class);
-                //startActivity(intent);
+                //ActivityOverview.this.startActivity(new Intent(ActivityOverview.this, ActivityPaymentOverview.class));
+                Intent intent = new Intent(getApplicationContext(), ActivityPaymentOverview.class);
+                startActivity(intent);
 
             }
 
