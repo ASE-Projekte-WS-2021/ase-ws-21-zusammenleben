513add7b883a59dcb237e14eba6a0e26d76141c8
sonasjoehnlein <jonas.soehnlein@stud.uni-regensburg.de>
2022-02-10 10:57:22 +0100

    prepare Login for Firebase

--- app/src/main/java/com/example/myapplication/ActivityLogin.java
+++ app/src/main/java/com/example/myapplication/ActivityLogin.java
@@ -13,52 +13,11 @@ import androidx.appcompat.app.AppCompatActivity;
 
 public class ActivityLogin extends AppCompatActivity {
 
-    EditText username, password;
-    TextView signup;
-    Button button_login;
-    LoginDBHelper UserDB;
-
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
 
-        username = (EditText) findViewById(R.id.login_username);
-        password = (EditText) findViewById(R.id.login_password);
-        signup = (TextView) findViewById(R.id.login_signup);
-        button_login = (Button) findViewById(R.id.btn_login);
-        UserDB = new LoginDBHelper(this);
-
-        signup.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                Intent intent = new Intent(getApplicationContext(),ActivitySignup.class);
-                startActivity(intent);
-            }
-        });
-
-        button_login.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-
-                String user = username.getText().toString();
-                String pass = password.getText().toString();
-
-                if(user.equals("")||pass.equals(""))
-                    Toast.makeText(ActivityLogin.this, "Please enter your data.", Toast.LENGTH_SHORT).show();
-                else{
-                    Boolean checkuserpass = UserDB.checkusernamepassword(user, pass);
-                    if(checkuserpass==true){
-                        Toast.makeText(ActivityLogin.this, "Login successful!", Toast.LENGTH_SHORT).show();
-                        Intent intent = new Intent(getApplicationContext(), ActivityStartScreen.class);
-                        startActivity(intent);
-                    } else {
-                        Toast.makeText(ActivityLogin.this, "Data invalid!", Toast.LENGTH_SHORT).show();
-                    }
-                }
-            }
-        });
 
     }
 }
