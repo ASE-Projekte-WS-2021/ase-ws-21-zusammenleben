12a1e43ef66021a33236da37d40606ba3837d1aa
sonasjoehnlein <jonas.soehnlein@stud.uni-regensburg.de>
2022-02-10 12:41:05 +0100

    check if User is already signed in

--- app/src/main/java/com/example/myapplication/ActivityStartScreen.java
+++ app/src/main/java/com/example/myapplication/ActivityStartScreen.java
@@ -14,18 +14,25 @@ import android.text.TextUtils;
 import android.widget.EditText;
 import android.widget.ListView;
 
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
 import java.util.List;
 
 public class ActivityStartScreen extends AppCompatActivity {
 
 
     Button button_newPayment;
+    FirebaseAuth firebaseAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         //try to setup and test my stuff
         setContentView(R.layout.activity_startscreen);
+
+        firebaseAuth = FirebaseAuth.getInstance();
+
         button_newPayment = (Button) findViewById(R.id.btn_newPayment);
 
         button_newPayment.setOnClickListener(new View.OnClickListener() {
@@ -37,4 +44,25 @@ public class ActivityStartScreen extends AppCompatActivity {
             }
         });
     }
+
+    private void checkUserStatus (){
+        FirebaseUser user = firebaseAuth.getCurrentUser();
+
+        if (user != null){
+
+
+
+        } else {
+            startActivity(new Intent(ActivityStartScreen.this, ActivityLogin.class));
+            finish();
+        }
+    }
+
+    @Override
+    protected void onStart() {
+        checkUserStatus();
+        super.onStart();
+    }
+
+
 }
