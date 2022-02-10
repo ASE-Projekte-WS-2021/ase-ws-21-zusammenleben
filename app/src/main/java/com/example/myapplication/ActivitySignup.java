88d9dbf659c315ab3e10c5471bb6cdc8cc9e39e5
sonasjoehnlein <jonas.soehnlein@stud.uni-regensburg.de>
2022-02-10 11:56:43 +0100

    fill registerUser Method

--- app/src/main/java/com/example/myapplication/ActivitySignup.java
+++ app/src/main/java/com/example/myapplication/ActivitySignup.java
@@ -11,13 +11,24 @@ import android.widget.EditText;
 import android.widget.TextView;
 import android.widget.Toast;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
+
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.Task;
+import com.google.firebase.auth.AuthResult;
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
 
 public class ActivitySignup extends AppCompatActivity {
 
     EditText signupemail, signuppassword;
     Button createAccount;
+
+    //declare instance FirebaseAuth
+    private FirebaseAuth mAuth;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +40,9 @@ public class ActivitySignup extends AppCompatActivity {
         //implement BackButton
         actionBar.setDisplayHomeAsUpEnabled(true);
         actionBar.setDisplayShowHomeEnabled(true);
+
+        //initialize FirebaseAuth
+        mAuth = FirebaseAuth.getInstance();
 
         // init our views
         signupemail = findViewById(R.id.signup_e_mail);
@@ -59,6 +73,29 @@ public class ActivitySignup extends AppCompatActivity {
     }
 
     private void registerUser(String email, String password) {
+
+        mAuth.createUserWithEmailAndPassword(email, password)
+                .addOnCompleteListener(this, new OnCompleteListener<AuthResult>() {
+                    @Override
+                    public void onComplete(@NonNull Task<AuthResult> task) {
+                        if (task.isSuccessful()) {
+                            // Sign in success, start register activity
+                            FirebaseUser user = mAuth.getCurrentUser();
+                            Toast.makeText(ActivitySignup.this, "Registered...\n"+user.getEmail(), Toast.LENGTH_SHORT).show();
+                            startActivity(new Intent(ActivitySignup.this, ActivityStartScreen.class));
+                            finish();
+                        } else {
+                            // If sign in fails, display a message to the user.
+                            Toast.makeText(ActivitySignup.this, "Authentication failed.", Toast.LENGTH_SHORT).show();
+
+                        }
+                    }
+                }) .addOnFailureListener(new OnFailureListener() {
+            @Override
+            public void onFailure(@NonNull Exception e) {
+                Toast.makeText(ActivitySignup.this, ""+e.getMessage(), Toast.LENGTH_SHORT).show();
+            }
+        }) ;
     }
 
     @Override
