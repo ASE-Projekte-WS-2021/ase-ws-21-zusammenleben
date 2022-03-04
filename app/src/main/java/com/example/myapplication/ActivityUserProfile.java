b0f24334a9fbd7c760ff1e1ba891f1a43bbc6b85
sonasjoehnlein <jonas.soehnlein@stud.uni-regensburg.de>
2022-03-04 10:48:49 +0100

    upload/change profilepicture successfully

--- app/src/main/java/com/example/myapplication/ActivityUserProfile.java
+++ app/src/main/java/com/example/myapplication/ActivityUserProfile.java
@@ -6,21 +6,54 @@ import androidx.appcompat.app.ActionBar;
 import androidx.appcompat.app.AppCompatActivity;
 import androidx.fragment.app.Fragment;
 
+import android.annotation.SuppressLint;
+import android.app.ProgressDialog;
+import android.content.ActivityNotFoundException;
 import android.content.Context;
 import android.content.Intent;
+import android.graphics.Bitmap;
 import android.graphics.Color;
 import android.graphics.drawable.Drawable;
+import android.net.Uri;
+import android.nfc.Tag;
 import android.os.Bundle;
+import android.provider.MediaStore;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.MenuItem;
 import android.view.View;
 import android.view.ViewGroup;
 import android.widget.Button;
 import android.widget.TextView;
+import android.widget.Toast;
 
+import com.bumptech.glide.Glide;
+import com.google.android.gms.tasks.Continuation;
+import com.google.android.gms.tasks.OnCompleteListener;
+import com.google.android.gms.tasks.OnFailureListener;
+import com.google.android.gms.tasks.OnSuccessListener;
+import com.google.android.gms.tasks.Task;
 import com.google.android.material.bottomnavigation.BottomNavigationView;
+import com.google.android.material.snackbar.Snackbar;
 import com.google.firebase.auth.FirebaseAuth;
 import com.google.firebase.auth.FirebaseUser;
+import com.google.firebase.auth.UserProfileChangeRequest;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.storage.FirebaseStorage;
+import com.google.firebase.storage.OnProgressListener;
+import com.google.firebase.storage.StorageReference;
+import com.google.firebase.storage.StorageTask;
+import com.google.firebase.storage.UploadTask;
+import com.theartofdev.edmodo.cropper.CropImage;
+
+import java.io.ByteArrayOutputStream;
+import java.net.URI;
+import java.util.HashMap;
+import java.util.UUID;
+import java.util.concurrent.ThreadPoolExecutor;
+
+import de.hdodenhof.circleimageview.CircleImageView;
 
 public class ActivityUserProfile extends AppCompatActivity {
 
@@ -29,6 +62,8 @@ public class ActivityUserProfile extends AppCompatActivity {
     BottomNavigationView bottomNavigationView;
     FirebaseAuth firebaseAuth;
     TextView useremail;
+    int TAKE_IMAGE_CODE = 10001;
+    private CircleImageView circleImageView;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -42,9 +77,25 @@ public class ActivityUserProfile extends AppCompatActivity {
         button_signout = (Button) findViewById(R.id.btn_logout);
         button_leaving = (Button) findViewById(R.id.btn_leaving);
         useremail = findViewById(R.id.show_email);
+        circleImageView =findViewById(R.id.user_picture);
 
         bottomNavigationView = findViewById(R.id.bottomnavview);
         bottomNavigationView.setSelectedItemId(R.id.user);
+
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        if (user.getPhotoUrl() != null ){
+            Glide.with(this)
+                    .load(user.getPhotoUrl())
+                    .into(circleImageView);
+        }
+
+        circleImageView.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                setpicture();
+            }
+        });
 
         bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
             @Override
@@ -92,6 +143,102 @@ public class ActivityUserProfile extends AppCompatActivity {
             finish();
         }
     }
+
+    private void setpicture(){
+        Intent intent = new Intent(MediaStore.ACTION_IMAGE_CAPTURE);
+        try {
+            startActivityForResult(intent, TAKE_IMAGE_CODE);
+        } catch (ActivityNotFoundException e) {
+            Toast.makeText(ActivityUserProfile.this, "Activity not found", Toast.LENGTH_SHORT).show();
+        }
+
+    }
+
+    @Override
+    protected void onActivityResult(int requestCode, int resultCode, @Nullable Intent data) {
+        super.onActivityResult(requestCode, resultCode, data);
+        if (requestCode == TAKE_IMAGE_CODE){
+
+            switch (resultCode){
+                case RESULT_OK:
+                    Bitmap bitmap = (Bitmap) data.getExtras().get("data");
+                    circleImageView.setImageBitmap(bitmap);
+                    uploadimage(bitmap);
+            }
+        }
+    }
+
+    private void uploadimage(Bitmap bitmap) {
+
+        ByteArrayOutputStream byteArrayOutputStream = new ByteArrayOutputStream();
+        bitmap.compress(Bitmap.CompressFormat.JPEG, 100,byteArrayOutputStream);
+
+        String uid = FirebaseAuth.getInstance().getCurrentUser().getUid();
+
+        StorageReference reference = FirebaseStorage.getInstance().getReference()
+                .child("images")
+                .child(uid + ".jpeg");
+        reference.putBytes(byteArrayOutputStream.toByteArray())
+
+                //final ProgressDialog pd = new ProgressDialog(this);
+                //pd.setTitle("Upload");
+                //pd.show();
+                .addOnSuccessListener(new OnSuccessListener<UploadTask.TaskSnapshot>() {
+                    @Override
+                    public void onSuccess(UploadTask.TaskSnapshot taskSnapshot) {
+                        //pd.dismiss();
+                        getDownloadUrl(reference);
+                        Snackbar.make(findViewById(android.R.id.content), "Picture successfully changed", Snackbar.LENGTH_SHORT).show();
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        //pd.dismiss();
+                        Toast.makeText(getApplicationContext(), "Failed", Toast.LENGTH_LONG).show();
+                    }
+                });
+
+    }
+
+    private void getDownloadUrl(StorageReference reference){
+        reference.getDownloadUrl()
+                .addOnSuccessListener(new OnSuccessListener<Uri>() {
+                    @Override
+                    public void onSuccess(Uri uri) {
+                        //Log.d(TAG, "onSuccess:" + uri);
+                        Toast.makeText(ActivityUserProfile.this, "Succesfully", Toast.LENGTH_SHORT).show();
+                        setUserProfileUrl(uri);
+                    }
+                });
+
+    }
+
+    private void setUserProfileUrl(Uri uri){
+        FirebaseUser user = FirebaseAuth.getInstance().getCurrentUser();
+
+        UserProfileChangeRequest request = new UserProfileChangeRequest.Builder()
+                .setPhotoUri(uri)
+                .build();
+
+        user.updateProfile(request)
+                .addOnSuccessListener(new OnSuccessListener<Void>() {
+                    @Override
+                    public void onSuccess(Void unused) {
+                        Toast.makeText(ActivityUserProfile.this, "Succesfully", Toast.LENGTH_SHORT).show();
+
+                    }
+                })
+                .addOnFailureListener(new OnFailureListener() {
+                    @Override
+                    public void onFailure(@NonNull Exception e) {
+                        Toast.makeText(ActivityUserProfile.this, "Failed", Toast.LENGTH_SHORT).show();
+                    }
+                });
+
+    }
+
+
 
     @Override
     protected void onStart() {
