3c923a6da6e806db0d116b459ebbe008ef442905
Basti Schwarz <53933323+bastischwarz-96@users.noreply.github.com>
2022-03-19 11:28:04 +0100

    list update

--- app/src/main/java/com/example/myapplication/ActivityShoppingList.java
+++ app/src/main/java/com/example/myapplication/ActivityShoppingList.java
@@ -1,21 +1,19 @@
 package com.example.myapplication;
 
 
-import android.content.DialogInterface;
 import android.content.Intent;
 import android.os.Bundle;
 import android.view.LayoutInflater;
 import android.view.Menu;
 import android.view.MenuItem;
 import android.view.View;
-import android.widget.AdapterView;
 import android.widget.ArrayAdapter;
 import android.widget.EditText;
 import android.widget.ListView;
 import android.widget.PopupMenu;
+import android.widget.TextView;
 import android.widget.Toast;
 
-import androidx.annotation.NonNull;
 import androidx.appcompat.app.AlertDialog;
 import androidx.appcompat.app.AppCompatActivity;
 
@@ -27,10 +25,13 @@ public class ActivityShoppingList extends AppCompatActivity {
 
     //array list for data
     ArrayList<String> list = new ArrayList<>();
-    ListView list_view;
-    ArrayAdapter arrayAdapter;
+    ArrayList<String> listcosts = new ArrayList<>();
+    ListView list_view, list_view_cost;
+    ArrayAdapter arrayAdapter, arrayAdapterCosts;
 
     BottomNavigationView bottomNavigationView;
+
+    TextView sumCosts;
 
 
     @Override
@@ -39,105 +40,149 @@ public class ActivityShoppingList extends AppCompatActivity {
         setContentView(R.layout.activity_shoppinglist);
         //find view by id
         list_view = findViewById(R.id.list_view);
-        arrayAdapter = new ArrayAdapter(ActivityShoppingList.this, android.R.layout.simple_list_item_1, list);
-        list_view.setAdapter(arrayAdapter);
+        list_view_cost = findViewById(R.id.list_view_cost);
+        sumCosts = findViewById(R.id.sumcosts);
 
-        //
+        arrayAdapter = new ArrayAdapter(ActivityShoppingList.this, android.R.layout.simple_list_item_1, list);
+        arrayAdapterCosts = new ArrayAdapter(ActivityShoppingList.this, android.R.layout.simple_list_item_1, listcosts);
+        list_view.setAdapter(arrayAdapter);
+        list_view_cost.setAdapter(arrayAdapterCosts);
+
+
         bottomNavigationView = findViewById(R.id.bottomnavview);
         bottomNavigationView.setSelectedItemId(R.id.shopping);
 
-        bottomNavigationView.setOnNavigationItemSelectedListener(new BottomNavigationView.OnNavigationItemSelectedListener() {
-            @Override
-            public boolean onNavigationItemSelected(@NonNull MenuItem item) {
-                switch(item.getItemId()){
-                    case R.id.payment:
-                        startActivity(new Intent(getApplicationContext(),ActivityOverview.class));
-                        overridePendingTransition(0,0);
-                        return true;
-                    case R.id.home:
-                         startActivity(new Intent(getApplicationContext(),ActivityStartScreen.class));
-                         overridePendingTransition(0,0);
+        bottomNavigationView.setOnNavigationItemSelectedListener(item -> {
+            switch(item.getItemId()){
+                case R.id.payment:
+                    startActivity(new Intent(getApplicationContext(),ActivityOverview.class));
+                    overridePendingTransition(0,0);
                     return true;
-                    case R.id.shopping:
-                    return true;
-                    case R.id.user:
-                        startActivity(new Intent(getApplicationContext(),ActivityUserProfile.class));
-                        overridePendingTransition(0,0);
-                    return true;
-                }
-                return false;
+                case R.id.home:
+                     startActivity(new Intent(getApplicationContext(),ActivityStartScreen.class));
+                     overridePendingTransition(0,0);
+                return true;
+                case R.id.shopping:
+                return true;
+                case R.id.user:
+                    startActivity(new Intent(getApplicationContext(),ActivityUserProfile.class));
+                    overridePendingTransition(0,0);
+                return true;
             }
+            return false;
         });
+        initClickListeners();
+    }
 
-        list_view.setOnItemClickListener(new AdapterView.OnItemClickListener() {
-            @Override
-            public void onItemClick(AdapterView<?> parent, View view, final int position, long id) {
+    private void initClickListeners(){
+        list_view.setOnItemClickListener((parent, view, position, id) -> {
 
-                PopupMenu popupMenu = new PopupMenu(ActivityShoppingList.this, view);
-                popupMenu.getMenuInflater().inflate(R.menu.pop_up_menu, popupMenu.getMenu());
+            PopupMenu popupMenu = new PopupMenu(ActivityShoppingList.this, view);
+            popupMenu.getMenuInflater().inflate(R.menu.pop_up_menu, popupMenu.getMenu());
 
-                popupMenu.setOnMenuItemClickListener(new PopupMenu.OnMenuItemClickListener() {
-                    @Override
-                    public boolean onMenuItemClick(MenuItem item) {
+            popupMenu.setOnMenuItemClickListener(item -> {
 
-                        switch (item.getItemId()) {
+                switch (item.getItemId()) {
 
-                            case R.id.item_update:
-                                //function for update
-                                AlertDialog.Builder builder = new AlertDialog.Builder(ActivityShoppingList.this);
-                                View v = LayoutInflater.from(ActivityShoppingList.this).inflate(R.layout.item_dialog, null, false);
-                                builder.setTitle("Update Item");
-                                final EditText editText = v.findViewById(R.id.etItem);
-                                editText.setText(list.get(position));
+                    case R.id.item_update:
+                        //function for update
+                        AlertDialog.Builder builder = new AlertDialog.Builder(ActivityShoppingList.this);
+                        View view1 = LayoutInflater.from(ActivityShoppingList.this).inflate(R.layout.item_dialog, null, false);
+                        builder.setTitle("Update Item");
+                        final EditText editText = view1.findViewById(R.id.etItem);
+                        editText.setText(list.get(position));
 
-                                //set custome view to dialog
-                                builder.setView(v);
 
-                                builder.setPositiveButton("Update", new DialogInterface.OnClickListener() {
-                                    @Override
-                                    public void onClick(DialogInterface dialog, int which) {
-                                        if (!editText.getText().toString().isEmpty()) {
-                                            list.set(position, editText.getText().toString().trim());
-                                            arrayAdapter.notifyDataSetChanged();
-                                            Toast.makeText(ActivityShoppingList.this, "Item Updated!", Toast.LENGTH_SHORT).show();
+                        //set custome view to dialog
+                        builder.setView(view1);
 
-                                        } else {
-                                            editText.setError("add item here !");
-                                        }
-                                    }
-                                });
-
-                                builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-                                    @Override
-                                    public void onClick(DialogInterface dialog, int which) {
-                                        dialog.dismiss();
-                                    }
-                                });
-
-                                builder.show();
-
-                                break;
-
-                            case R.id.item_del:
-                                //fucntion for del
-                                Toast.makeText(ActivityShoppingList.this, "Item Deleted", Toast.LENGTH_SHORT).show();
-                                list.remove(position);
+                        builder.setPositiveButton("Update", (dialog, which) -> {
+                            if (!editText.getText().toString().isEmpty()) {
+                                list.set(position, editText.getText().toString().trim());
                                 arrayAdapter.notifyDataSetChanged();
+                                Toast.makeText(ActivityShoppingList.this, "Item Updated!", Toast.LENGTH_SHORT).show();
 
-                                break;
+                            }
+                            else {
+                                editText.setError("Füge hier das Element ein!");
+                            }
+                        });
 
-                        }
+                        builder.setNegativeButton("Abbrechen", (dialog, which) -> dialog.dismiss());
 
-                        return true;
-                    }
-                });
+                        builder.show();
 
-                //don't forgot this
-                popupMenu.show();
+                        break;
 
-            }
+                    case R.id.item_del:
+                        //function for delete
+                        Toast.makeText(ActivityShoppingList.this, "Das Element wurde entfernt", Toast.LENGTH_SHORT).show();
+                        list.remove(position);
+                        listcosts.remove(position);
+                        arrayAdapter.notifyDataSetChanged();
+
+                        break;
+
+                }
+
+                return true;
+            });
+            popupMenu.show();
         });
 
+        list_view_cost.setOnItemClickListener((parent, view, position, id) -> {
+
+            PopupMenu popupMenu = new PopupMenu(ActivityShoppingList.this, view);
+            popupMenu.getMenuInflater().inflate(R.menu.pop_up_menu, popupMenu.getMenu());
+
+            popupMenu.setOnMenuItemClickListener(item -> {
+
+                switch (item.getItemId()) {
+
+                    case R.id.item_update:
+                        //function for update
+                        AlertDialog.Builder builder = new AlertDialog.Builder(ActivityShoppingList.this);
+                        View view1 = LayoutInflater.from(ActivityShoppingList.this).inflate(R.layout.item_dialog, null, false);
+                        builder.setTitle("Update Cost");
+                        final EditText costItem= view1.findViewById(R.id.costItem);
+                        costItem.setText(listcosts.get(position));
+
+                        //set custome view to dialog
+                        builder.setView(view1);
+
+                        builder.setPositiveButton("Update", (dialog, which) -> {
+                            if ( !costItem.getText().toString().isEmpty()) {
+                                listcosts.set(position, costItem.getText().toString().trim());
+                                arrayAdapterCosts.notifyDataSetChanged();
+                                Toast.makeText(ActivityShoppingList.this, "Item Updated!", Toast.LENGTH_SHORT).show();
+
+                            }
+                            else {
+                                costItem.setError("Füge hier den Preis ein!");
+                            }
+                        });
+
+                        builder.setNegativeButton("Abbrechen", (dialog, which) -> dialog.dismiss());
+
+                        builder.show();
+
+                        break;
+
+                    case R.id.item_del:
+                        //function for delete
+                        Toast.makeText(ActivityShoppingList.this, "Das Element wurde entfernt", Toast.LENGTH_SHORT).show();
+                        listcosts.remove(position);
+                        list.remove(position);
+                        arrayAdapterCosts.notifyDataSetChanged();
+
+                        break;
+
+                }
+
+                return true;
+            });
+            popupMenu.show();
+        });
     }
 
     @Override
@@ -154,7 +199,7 @@ public class ActivityShoppingList extends AppCompatActivity {
 
             case R.id.add_item:
                 //function to add
-                _addItem();
+                addItem();
                 break;
 
         }
@@ -165,7 +210,7 @@ public class ActivityShoppingList extends AppCompatActivity {
     /*
      * method for adding item
      * */
-    private void _addItem() {
+    private void addItem() {
 
         AlertDialog.Builder builder = new AlertDialog.Builder(ActivityShoppingList.this);
         builder.setTitle("Add New Item");
@@ -174,27 +219,32 @@ public class ActivityShoppingList extends AppCompatActivity {
 
         builder.setView(v);
         final EditText etItem = v.findViewById(R.id.etItem);
-        builder.setPositiveButton("Add", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                if (!etItem.getText().toString().isEmpty()) {
-                    list.add(etItem.getText().toString().trim());
-                    arrayAdapter.notifyDataSetChanged();
+        final EditText costItem = v.findViewById(R.id.costItem);
+        builder.setPositiveButton("Add", (dialog, which) -> {
+            if (!etItem.getText().toString().isEmpty() && !costItem.getText().toString().isEmpty()) {
+                list.add(etItem.getText().toString().trim());
+                listcosts.add(costItem.getText().toString().trim());
+                arrayAdapter.notifyDataSetChanged();
+                arrayAdapterCosts.notifyDataSetChanged();
+                addSums();
 
-                } else {
-                    etItem.setError("add item here !");
-                }
+            } else {
+                etItem.setError("add item here !");
+                costItem.setError("add item here !");
             }
         });
 
-        builder.setNegativeButton("Cancel", new DialogInterface.OnClickListener() {
-            @Override
-            public void onClick(DialogInterface dialog, int which) {
-                dialog.dismiss();
-            }
-        });
+        builder.setNegativeButton("Cancel", (dialog, which) -> dialog.dismiss());
 
         builder.show();
 
+    }
+
+    private void addSums(){
+        /*double total = 0.0;
+        for (int i =0; i < listcosts.size(); i++){
+            total = Double.parseDouble(total + listcosts.get(i));
+        }*/
+        sumCosts.setText("HI");
     }
 }
