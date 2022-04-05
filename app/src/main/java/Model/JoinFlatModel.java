08431ea4828bdaac52df07fe48a1b5d85f81a1c1
MarcoBeetz23 <57911367+MarcoBeetz23@users.noreply.github.com>
2022-04-05 13:38:34 +0200

    abc

--- app/src/main/java/Model/JoinFlatModel.java
+++ app/src/main/java/Model/JoinFlatModel.java
@@ -44,7 +44,7 @@ public class JoinFlatModel implements JoinFlatContract.Model, JoinFlatContract.o
                     String address = snap.getValue(Flat.class).getAddress();
                     Flat flat = new Flat(address, id, members, size);
                     flats.add(flat);
-                    // test
+                    // abc
                 }
                 onJoinFlatListener.onSuccess(flats);
             }
