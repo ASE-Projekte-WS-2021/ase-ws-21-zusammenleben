8c906c90a185374abea1062606013501d64c1080
sonasjoehnlein <jonas.soehnlein@stud.uni-regensburg.de>
2022-04-14 10:06:06 +0200

    check duplicate email

--- app/src/main/java/View/Before/RegistrationActivity.java
+++ app/src/main/java/View/Before/RegistrationActivity.java
@@ -4,6 +4,7 @@ import android.content.Intent;
 import android.os.Bundle;
 import android.text.TextUtils;
 import android.util.Log;
+import android.util.Patterns;
 import android.view.View;
 import android.widget.Button;
 import android.widget.EditText;
@@ -52,25 +53,41 @@ public class RegistrationActivity extends AppCompatActivity implements View.OnCl
     }
 
     private void checkRegistrationDetails(){
-        if(!TextUtils.isEmpty(signUpEmail.getText().toString()) && !TextUtils.isEmpty(signUpPassword.getText().toString())){
+
+        if (!Patterns.EMAIL_ADDRESS.matcher(signUpEmail.getText().toString()).matches() || TextUtils.isEmpty(signUpEmail.getText().toString())) {
+            signUpEmail.setError("Bitte geben Sie ihre korrekte E-Mail an.");
+            signUpEmail.setFocusable(true);
+        } else if (TextUtils.isEmpty(signUpName.getText().toString())){
+            signUpName.setError("Bitte geben Sie ihren Name an.");
+            signUpName.setFocusable(true);
+        } else if (TextUtils.isEmpty(signUpPassword.getText().toString()) || signUpPassword.getText().toString().length()<6){
+            signUpPassword.setError("Bitte geben Sie ihr Passwort mit mehr als 6 Zeichen  an.");
+            signUpPassword.setFocusable(true);
+        } else {
             initLogin(signUpEmail.getText().toString(), signUpPassword.getText().toString(), signUpName.getText().toString());
+        }
+
+        /*if(!TextUtils.isEmpty(signUpEmail.getText().toString()) && !TextUtils.isEmpty(signUpPassword.getText().toString())){
+          initLogin(signUpEmail.getText().toString(), signUpPassword.getText().toString(), signUpName.getText().toString());
         } else {
             signUpEmail.setError("Bitte geben Sie ihre E-Mail an.");
             signUpEmail.setFocusable(true);
             signUpPassword.setError("Bitte geben Sie ihr Passwort an.");
             signUpPassword.setFocusable(true);
-        }
+        }*/
     }
 
     private void initLogin(String email, String password, String name){
         mRegistrationPresenter.register(this, email, password, name);
-        Intent i = new Intent(RegistrationActivity.this, CreateFlatActivity.class);
-        startActivity(i);
+        //Intent i = new Intent(RegistrationActivity.this, CreateFlatActivity.class);
+        //startActivity(i);
     }
 
     @Override
     public void onRegistrationSuccess(FirebaseUser firebaseUser) {
         Toast.makeText(getApplicationContext(), "Ihr Konto wurde erfolgreich erstellt", Toast.LENGTH_SHORT).show();
+        Intent i = new Intent(RegistrationActivity.this, CreateFlatActivity.class);
+        startActivity(i);
     }
 
     @Override
